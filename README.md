56e30199c41f0048b43501802a34f004608f8a2a
sebastian.schmidt <bastman77@gmail.com>
2021-07-16 15:06:19 +0200

    initial

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dgs-playground
 
 see: https://github.com/Netflix/dgs-examples-kotlin
+
+http://localhost:8080/graphiql
--- app/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ app/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,7 +1,15 @@
 package com.example.demo
 
+import com.example.demo.generated.types.Show
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsQuery
+import com.netflix.graphql.dgs.DgsRuntimeWiring
+import com.netflix.graphql.dgs.InputArgument
+import graphql.scalars.ExtendedScalars
+import graphql.schema.idl.RuntimeWiring
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 class DemoApplication
@@ -10,3 +18,40 @@ fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
 
+@Service
+class ShowsService {
+    fun shows(): List<Show> {
+        return listOf(
+            Show(id = 1, title = "Stranger Things", releaseYear = 2016),
+            Show(id = 2, title = "Ozark", releaseYear = 2017),
+            Show(id = 3, title = "The Crown", releaseYear = 2016),
+            Show(id = 4, title = "Dead to Me", releaseYear = 2019),
+            Show(id = 5, title = "Orange is the New Black", releaseYear = 2013)
+        )
+    }
+}
+
+@DgsComponent
+class DateTimeScalarRegistration {
+
+    @DgsRuntimeWiring
+    fun addScalar(builder: RuntimeWiring.Builder): RuntimeWiring.Builder {
+        return builder.scalar(ExtendedScalars.DateTime)
+    }
+}
+
+@DgsComponent
+class ShowsDataFetcher(private val showsService: ShowsService) {
+    /**
+     * This datafetcher resolves the shows field on Query.
+     * It uses an @InputArgument to get the titleFilter from the Query if one is defined.
+     */
+    @DgsQuery
+    fun shows(@InputArgument titleFilter : String?): List<Show> {
+        return if(titleFilter != null) {
+            showsService.shows().filter { it.title.contains(titleFilter) }
+        } else {
+            showsService.shows()
+        }
+    }
+}
